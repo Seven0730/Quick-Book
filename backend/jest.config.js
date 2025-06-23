bbd72c37810374f8e28db96e8b48705c0433a4d8
Seven0730 <hanhai0730@gmail.com>
2025-06-24 00:43:55 +0800

    jest env config

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  clearMocks: true,
+};
